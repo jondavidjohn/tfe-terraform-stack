dcd74d9a2557874d6a547a113e1708d508a84e4d
Jonathan Johnson <me@jondavidjohn.com>
2025-02-06 15:58:33 -0600

    try again

--- teams/main.tf
+++ teams/main.tf
@@ -22,6 +22,39 @@ variable "workspace_ids" {
   type = set(string)
 }
 
+
+locals {
+  visible_team_access_set = setproduct(var.workspace_ids, toset([for t in tfe_team.visible_teams : t.id]))
+  visible_team_access_map = { for a in local.visible_team_access_set : "${a[0]}-${a[1]}" => {
+    "workspace_id" = a[0],
+    "team_id" = a[1],
+  }}
+
+  secret_team_access_set  = setproduct(var.workspace_ids, toset([for t in tfe_team.secret_teams : t.id]))
+  secret_team_access_map = { for a in local.secret_team_access_set : "${a[0]}-${a[1]}" => {
+    "workspace_id" = a[0],
+    "team_id" = a[1],
+  }}
+
+  visible_team_membership_set = setproduct(
+    toset([for m in data.tfe_organization_membership.visible_team_members : m.id]),
+    toset([for t in tfe_team.visible_teams : t.id])
+  )
+  visible_team_membership_map = { for a in local.visible_team_membership_set : "${a[0]}-${a[1]}" => {
+    "membership_id" = a[0],
+    "team_id" = a[1],
+  }}
+
+  secret_team_membership_set = setproduct(
+    toset([for m in data.tfe_organization_membership.secret_team_members : m.id]),
+    toset([for t in tfe_team.secret_teams : t.id])
+  )
+  secret_team_membership_map = { for a in local.secret_team_membership_set : "${a[0]}-${a[1]}" => {
+    "membership_id" = a[0],
+    "team_id" = a[1],
+  }}
+}
+
 data "tfe_organization_membership" "visible_team_members" {
   for_each      = var.visible_team_users
   organization  = var.organization_name
@@ -41,11 +74,11 @@ resource "tfe_team" "visible_teams" {
   visibility   = "organization"
 }
 
-resource "tfe_team_access" "team_workspace_access" {
-  for_each     = setproduct(var.workspace_ids, toset([for t in tfe_team.visible_teams : t.id]))
+resource "tfe_team_access" "visible_team_workspace_access" {
+  for_each     = local.visible_team_access_map
   access       = "read"
-  workspace_id = each.value[0]
-  team_id      = each.value[1]
+  workspace_id = each.value.workspace_id
+  team_id      = each.value.team_id
 }
 
 resource "tfe_team" "secret_teams" {
@@ -56,26 +89,20 @@ resource "tfe_team" "secret_teams" {
 }
 
 resource "tfe_team_access" "secret_team_workspace_access" {
-  for_each     = setproduct(var.workspace_ids, toset([for t in tfe_team.secret_teams : t.id]))
+  for_each     = local.secret_team_access_map
   access       = "read"
-  workspace_id = each.value[0]
-  team_id      = each.value[1]
+  workspace_id = each.value.workspace_id
+  team_id      = each.value.team_id
 }
 
 resource "tfe_team_organization_member" "visible_team_user_membership" {
-  for_each                   = setproduct(
-    toset([for m in data.tfe_organization_membership.visible_team_members : m.id]),
-    toset([for t in tfe_team.visible_teams : t.id])
-  )
-  organization_membership_id = each.value[0]
-  team_id                    = each.value[1]
+  for_each                   = local.visible_team_membership_map
+  organization_membership_id = each.value.membership_id
+  team_id                    = each.value.team_id
 }
 
 resource "tfe_team_organization_member" "secret_team_user_membership" {
-  for_each                   = setproduct(
-    toset([for m in data.tfe_organization_membership.visible_team_members : m.id]),
-    toset([for t in tfe_team.secret_teams : t.id])
-  )
-  organization_membership_id = each.value[0]
-  team_id                    = each.value[1]
+  for_each                   = local.secret_team_membership_map
+  organization_membership_id = each.value.membership_id
+  team_id                    = each.value.team_id
 }
